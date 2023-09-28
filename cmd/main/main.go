fc091db9e085b08b824c3b2a48ee02275b62c4b1
Margarita <altsybeevam@yandex.ru>
2023-09-28 12:23:06 +0300

    .

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"awesomeProject/internal/app/pkg"
 	"log"
 )
 
 func main() {
-	application = app.New()
-	application.Run()
+	// application = app.New()
+	// application.Run()
 	log.Println("Application start!")
 	log.Println("Application terminated!")
 }
